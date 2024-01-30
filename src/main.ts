92df405825ad34eb11bb9ea017959c03c8cccf45
Fuji-Noobie-Ghost <fitahiana.ratia@gmail.com>
2024-01-30 21:28:00 +0300

    feat: main shortify function

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { createHash } from 'node:crypto'
 import express, { Request, Response } from 'express'
 import { config } from 'dotenv'
 
@@ -9,8 +10,25 @@ const HOST = process.env.HOST || 'localhost'
 const app = express()
 app.use(express.json())
 
+function shortify(url: string, length = 7): Promise<string> {
+    return new Promise<string>((resolve) => {
+        resolve(createHash('md5')
+            .update(url + Date.now())
+            .digest('base64')
+            .slice(0, length)
+        )
+    })
+}
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, World!')
+})
+
+app.post('/shortify', async (req: Request, res: Response) => {
+    res.json({
+        originalURL: req.body.url,
+        shortenedURL: await shortify(req.body.url)
+    })
 })
 
 app.listen(parseInt(PORT), HOST, () => {
